04f2ed41a3051543a269d521cc9e56ce74e446da
jchacun <jeremy.chacun@reseau.eseo.Fr>
2023-03-27 11:44:25 +0200

    sg
    sg

--- app/src/main/java/com/example/minesweeper/StartGame.java
+++ app/src/main/java/com/example/minesweeper/StartGame.java
@@ -2,16 +2,27 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.minesweeper.databinding.ActivityMainBinding;
+import com.example.minesweeper.databinding.ActivityStartGameBinding;
 
 public class StartGame extends AppCompatActivity {
-    private ActivityMainBinding binding;
+    private ActivityStartGameBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityStartGameBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.sgButtonEasy.setOnClickListener(v -> {
+            Intent intent = new Intent(StartGame.this,MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
