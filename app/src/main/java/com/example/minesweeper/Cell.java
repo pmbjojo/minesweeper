cd6a35df265c4cc6288643a3a043d692b6f600f6
pmbjojo <jourdamj@gmail.com>
2023-03-27 13:41:35 +0200

    game engine init

---
+++ app/src/main/java/com/example/minesweeper/Cell.java
@@ -0,0 +1,47 @@
+package com.example.minesweeper;
+
+public class Cell {
+    private boolean isMine;
+    private boolean isFlagged;
+    private boolean isRevealed;
+    private int adjacentMines;
+
+    public Cell() {
+        isMine = false;
+        isFlagged = false;
+        isRevealed = false;
+        adjacentMines = 0;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        isRevealed = revealed;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+}
--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.minesweeper.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnCellClickListener {
     private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public void onCellClick(Cell cell) {
+        Toast.makeText(this, "Cell clicked", Toast.LENGTH_SHORT).show();
     }
 }
