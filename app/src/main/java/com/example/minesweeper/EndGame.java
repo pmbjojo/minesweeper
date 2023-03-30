3b26a25344c6764a70952b7fc5552e6e23929db5
Lucas <lucas.rolland@reseau.eseo.fr>
2023-03-30 09:13:55 +0200

    modification endgame

--- app/src/main/java/com/example/minesweeper/EndGame.java
+++ app/src/main/java/com/example/minesweeper/EndGame.java
@@ -2,13 +2,30 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.minesweeper.databinding.ActivityEndGameBinding;
+import com.example.minesweeper.databinding.ActivityMainBinding;
 
 public class EndGame extends AppCompatActivity {
 
+    private ActivityEndGameBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+        binding = ActivityEndGameBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intentEG = new Intent(EndGame.this, StartGame.class);
+        Bundle bundle = new Bundle();
+        binding.egHomebutton.setOnClickListener(v -> {
+            intentEG.putExtras(bundle);
+            startActivity(intentEG);
+        });
     }
 }
